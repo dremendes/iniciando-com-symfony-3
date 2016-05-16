540e9d75c3135044d9204fb2cabbd6978f8720d5
André Luiz <andre.mendesc@gmail.com>
2016-05-16 09:43:33 -0400

    done http://tableless.com.br/iniciando-com-symfony-2-parte-02/

---
+++ src/Tableless/CoreBundle/Controller/DefaultController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Tableless\CoreBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/")
+     */
+    public function indexAction()
+    {
+        return $this->render('TablelessCoreBundle:Default:index.html.twig');
+    }
+}
